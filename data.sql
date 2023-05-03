b05b4de85bf242d2ab39003b3cac631332c6a5f9
Precious Betine <preciousbetine@gmail.com>
2023-05-03 08:52:14 +0100

    Update animals species

--- data.sql
+++ data.sql
@@ -46,3 +46,11 @@ INSERT INTO owners (full_name, age) VALUES ('Jodie Whittaker', 38);
 
 INSERT INTO species (name) VALUES ('Pokemon');
 INSERT INTO species (name) VALUES ('Digimon');
+
+UPDATE animals
+SET species_id = (SELECT id FROM species WHERE name = 'Digimon')
+WHERE name LIKE '%mon';
+
+UPDATE animals
+SET species_id = (SELECT id FROM species WHERE name = 'Pokemon')
+WHERE species_id IS NULL;
