d759389a8a5ed49d0ca769c2bf2b08af7535331f
Precious Betine <preciousbetine@gmail.com>
2023-05-03 08:57:58 +0100

    Update animals' owners

--- data.sql
+++ data.sql
@@ -54,3 +54,23 @@ WHERE name LIKE '%mon';
 UPDATE animals
 SET species_id = (SELECT id FROM species WHERE name = 'Pokemon')
 WHERE species_id IS NULL;
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE name = 'Sam Smith')
+WHERE name = 'Agumon';
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE name = 'Jennifer Orwell')
+WHERE name IN ('Gabumon', 'Pikachu');
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE name = 'Bob')
+WHERE name IN ('Devimon', 'Plantmon');
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE name = 'Melody Pond')
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE name = 'Dean Winchester')
+WHERE name IN ('Angemon', 'Boarmon');
